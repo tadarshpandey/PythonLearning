f15e72d8b5fa69f15431dc1ac1bc73331dfa191f
tadarsh <adarshsomeshwaralok@gmail.com>
2026-03-25 22:58:00 +0530

    js learning

--- JSCore/index.js
+++ JSCore/index.js
@@ -1,3 +1,6 @@
 /** JS is a programming language. We use it to give instructions to the computer */
 
+// console.log("Me ....Adarsh Pandey..... going to start learning JavaScript");
+
+
 console.log("Me ....Adarsh Pandey..... going to start learning JavaScript");
